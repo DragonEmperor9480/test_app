486722f1bf8fc264bbfd71bdec588b9dff7c1ad2
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-07-05 13:56:35 +0530

    Lets learn Basics

---
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.first
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val edt: EditText =findViewById(R.id.edtxt)
+        val button: Button = findViewById(R.id.btn)
+        val txtv: TextView = findViewById(R.id.text)
+
+        button.setOnClickListener {
+            val edtinfo = edt.text.toString()
+            Log.i("This is text from EditText",edtinfo)
+
+
+        }
+
+    }
+}
